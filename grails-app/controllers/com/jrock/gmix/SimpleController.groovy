0cfe5df392fc7580adfcba5fa9334efbd499021d
Bayo Erinle <berinle@gmail.com>
2010-08-10 16:59:04 -0400

    experimental code consuming a WS in grails

--- grails-app/controllers/com/jrock/gmix/SimpleController.groovy
+++ grails-app/controllers/com/jrock/gmix/SimpleController.groovy
@@ -1,11 +1,19 @@
 package com.jrock.gmix
 
+import groovyx.net.ws.WSClient
+
 class SimpleController {
 
     def index = { }
     
     def suggest = {
     	log.debug "doing suggeston..."
+    	
+    	def proxy = new WSClient("http://localhost:8090/redlight/services/home?wsdl", this.class.classLoader)
+    	proxy.initialize()
+    	proxy.soapCall()
+    	
 		redirect(uri:'/')
+		//redirect(url:'http://www.cnn.com')
     }
 }
